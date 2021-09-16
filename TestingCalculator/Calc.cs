5ace527502a9f926d5ae2da8c7241ab109ec7463
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-09-16 08:38:59 +0200

    Add project files.

---
+++ TestingCalculator/Calc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingCalculator
+{
+    class Calc
+    {
+        double nr1;
+        double nr2;
+        double[] numberArr = new double[5];
+
+        public Calc(double nr1, double nr2)
+        {
+            this.nr1 = nr1;
+            this.nr2 = nr2;
+        }
+
+        public Calc(double[] numberArr)
+        {
+            this.numberArr = numberArr;
+        }
+
+        public void Addition()
+        {
+            double sum = nr1 + nr2;
+            Console.WriteLine(sum);
+        }
+
+        /*public static double[] Addition (double[] numberArr)
+        {
+            int sum;
+        AskUserForNr();
+
+            for (int i = 0; i <additionArr.Length; i++)
+            {
+               sum += additionArr[i];
+            }
+           return sum;
+        }//Addition 
+public static double AskUserForNr(double[] additionArr)
+{
+    return double[] }
+ }*/
+
+    }//Calc
+}//namespace
---
+++ TestingCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestingCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Type your first number: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type your second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Calc addition = new Calc(num1, num2);
+            addition.Addition();
+        }
+    }
+}
